46da288f6c454ecccb543d6a1d2da9280df473a6
Anonymous <136981872@qq.com>
2023-05-25 09:42:46 +0800

    https://leetcode.cn/problems/move-zeroes/

--- algorithm_test_project/src/main/java/moveZeroes/problem/Solution2.java
+++ algorithm_test_project/src/main/java/moveZeroes/problem/Solution2.java
@@ -11,6 +11,9 @@ class Solution2 {
                  *
                  * 右指针左边直到左指针处均为零。
                  */
+
+                // 1. 如果left是0 ,那么交换没问题
+                // 2. 如果left不是0,那么第一次交换left和right交换的是相同的元素 , 此时left++,right++,相当于递归了
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
